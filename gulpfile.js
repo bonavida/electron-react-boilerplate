cd1d008eb118b9541f5ff2bde9443090e6f4cb40
Diego Bonavida <gecko.bona@gmail.com>
2019-03-03 18:48:11 +0100

    Second try

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,13 @@
 var gulp = require('gulp');
-var clean = require('gulp-clean');
-
-gulp.task('clean-dist', () => {
-  return gulp.src(['dist/*'], { read: false })
-  .pipe(clean());
-});
-
-gulp.task('clean-build', () => {
-  return gulp.src(['app/build/**/*', 'app/build'], { read: false })
-  .pipe(clean());
-});
 
 gulp.task('frontend', () => {
   return gulp.src(['app/build/**/*'])
-  .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('dist/build/html'));
 });
 
 gulp.task('electron', () => {
-  return gulp.src(['electron/main.js', 'electron/preload.js', 'electron/package.json'])
+  return gulp.src(['electron/**/*', '!electron/node_modules', '!electron/package-lock.json', 'electron/package.json'])
   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulp.series('clean-dist', 'frontend', 'clean-build', 'electron'));
+gulp.task('default', gulp.series('frontend', 'electron'));
