687fb4f2b266d1485f6c230fe106df1fa15ab912
Diego Bonavida <gecko.bona@gmail.com>
2019-03-29 20:46:29 +0100

    Changed text App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -18,7 +18,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/components/App/App.tsx</code> and save to reload.
           </p>
           <a
             className="App-link"
